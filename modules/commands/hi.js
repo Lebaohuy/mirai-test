7ea52baaa9e288525e4cd7000dec5018823e063a
Lebaohuy <77989902+Lebaohuy@users.noreply.github.com>
2021-06-12 09:47:50 +0700

    Create hi.js

---
+++ modules/commands/hi.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+module.exports.config = {
+name: "Hi",
+	version: "1.0.1",
+	hasPermssion: 0,
+	credits: "VanHung",
+	description: "Hi",
+	commandCategory: "Không cần dấu lệnh",
+	usages: "cache",
+	cooldowns: 5,
+};
+module.exports.event = function({ api, event }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("Hi")==0 || (event.body.indexOf("hi")==0) || (event.body.indexOf("Hello")==0) || (event.body.indexOf("hello")==0)) {
+		var msg = {
+				body: "Chào bạn chúc bạn một ngày tốt lành UwU",
+				attachment: fs.createReadStream(__dirname + `/cache/Hi.gif`)
+			}
+			return api.sendMessage(msg, threadID, messageID);
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+}
