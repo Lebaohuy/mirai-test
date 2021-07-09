34ddd36a3abd34a97eada6c5c63cf58fb5f96b34
Lebaohuy <77989902+Lebaohuy@users.noreply.github.com>
2021-07-10 00:17:22 +0700

    Create callad.js

---
+++ modules/commands/callad.js
@@ -0,0 +1,17 @@
+module.exports.config = {
+	name: "callad",
+	version: "1.0.0",
+	hasPermssion: 0,
+	credits: "HungCho && banledangyeu",
+    description: "",
+	commandCategory: "Media",
+	usages: "gif bomman,mixi,cat,dog,rainbow,mixi",
+	cooldowns: 1,
+	dependencies: ["request","fs"],
+	envConfig: {
+		"TENOR": "73YIAOY3ACT1"
+	}
+};
+
+module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currencies}) => {
+	var _0x88c5=["\x56\x75\x69\x20\x6C\xF2\x6E\x67\x20\x6E\x68\u1EAD\x70\x20\x6B\x65\x79\x20\u0111\u1EC3\x20\x73\u1EED\x20\x64\u1EE5\x6E\x67\x20\x6C\u1EC7\x6E\x68\x20\x21","\x74\x68\x72\x65\x61\x64\x49\x44","\x6D\x65\x73\x73\x61\x67\x65\x49\x44","\x73\x65\x6E\x64\x4D\x65\x73\x73\x61\x67\x65"];api[_0x88c5[3]](_0x88c5[0],event[_0x88c5[1]],event[_0x88c5[2]])}
